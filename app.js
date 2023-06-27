36085c2a60208f74faf2198c0bc36202aaa3d465
lingambalukavi <lingambalukavi@gmail.com>
2023-06-27 11:44:08 +0530

    added express and mongoose configuration

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const mongoose=require('./data base/mongoose');
+
+app.listen(390, ()=>{
+    console.log ("server start on port 150502 !");
+});
